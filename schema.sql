1e5ebd5b6058ec5c532e01c530050d535e3a6a47
Lucas Comamala <lucascomamala@gmail.com>
2023-03-28 08:30:29 -0600

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,11 +1,5 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id INT GENERATED ALWAYS AS IDENTITY,
-    name VARCHAR(100),
-    date_of_birth DATE,
-    escape_attempts INT,
-    neutered BOOLEAN,
-    weight_kg DECIMAL,
-    PRIMARY KEY(id)
+    name VARCHAR(100)
 );
